dd88f47aa09fd3e6dfa1a94bdc195053351770d3
ilya-uzun <ilya.uzun@bk.ru>
2021-11-26 21:32:57 +0500

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("js") version "1.5.10"
+    //id("org.jetbrains.kotlin.multiplatform") version "1.5.10"
 }
 
 group = "me.ilya"
@@ -13,6 +14,12 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.206-kotlin-1.5.10")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.206-kotlin-1.5.10")
+    implementation(npm("react", "17.0.1"))
+    implementation(npm("react-dom", "17.0.1"))
+
+    // Kotlin Styled (шаг 3)
+    implementation("org.jetbrains:kotlin-styled:5.2.1-pre.148-kotlin-1.4.21")
+    implementation(npm("styled-components", "~5.2.1"))
 }
 
 kotlin {
